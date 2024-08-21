28a37312117ff9df1ea59dd1419ee1105d93474b
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2024-08-21 17:46:37 -0300

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,20 +1,28 @@
 const oracledb = require('oracledb');
 
+oracledb.initOracleClient({ libDir: 'C:\\oracle\\instantclient_21_12'});
+
 async function run() {
   let connection;
 
   try {
     // Tenta estabelecer a conexão
+    // connection = await oracledb.getConnection({
+    //   user: 'system',
+    //   password: '159753',
+    //   connectString: 'localhost:1521/XE'  // Exemplo: 'localhost/XE' ou 'hostname:porta/SID'
+    // });
+
     connection = await oracledb.getConnection({
-      user: 'system',
-      password: '159753',
-      connectString: 'localhost:1521/XE'  // Exemplo: 'localhost/XE' ou 'hostname:porta/SID'
-    });
+        user: 'dbamv',
+        password: 'suporte@98huI*!',
+        connectString: '192.168.10.236:1521/sml'  // Exemplo: 'localhost/XE' ou 'hostname:porta/SID'
+      });
 
     console.log('Conexão bem-sucedida!');
 
     // Execute alguma consulta para testar
-    const result = await connection.execute(`SELECT 'Conexão bem-sucedida!' AS status FROM dual`);
+    const result = await connection.execute(`SELECT dt_atendimento FROM dbamv.atendime where cd_atendimento = 654`);
     console.log(result.rows[0][0]);
 
   } catch (err) {
