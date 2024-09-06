9d8eecea7ea4581ed267dd671bd2b4161e9b5b75
Jefferson Lima Gonçalves <46583320+jefflima11@users.noreply.github.com>
2024-09-06 10:52:26 -0300

    correction and create connection

--- connection.js
+++ connection.js
@@ -1,32 +1,29 @@
+// Necessario a biblioteca: npm install oracledb
+
 const oracledb = require('oracledb');
 
-oracledb.initOracleClient({ libDir: 'C:\\oracle\\instantclient_21_12'});
+const config = {
+  user: '',         
+  password: '',       
+  connectString: ''
+};
 
-async function run() {
+// Necessário para a versão atual do banco oracle, alterar para instancia oracle instalada
+oracledb.initOracleClient({ libDir: 'C:\\oracle\\instantclient_21_12' });
+
+async function connectToOracle() {
   let connection;
 
   try {
-    // Tenta estabelecer a conexão
-    // connection = await oracledb.getConnection({
-    //   user: 'system',
-    //   password: '159753',
-    //   connectString: 'localhost:1521/XE'  // Exemplo: 'localhost/XE' ou 'hostname:porta/SID'
-    // });
+    connection = await oracledb.getConnection(config);
+    console.log('Conectado ao Oracle!');
 
-    connection = await oracledb.getConnection({
-        user: 'dbamv',
-        password: 'suporte@98huI*!',
-        connectString: '192.168.10.236:1521/sml'  // Exemplo: 'localhost/XE' ou 'hostname:porta/SID'
-      });
-
-    console.log('Conexão bem-sucedida!');
-
-    // Execute alguma consulta para testar
-    const result = await connection.execute(`SELECT dt_atendimento, cd_atendimento FROM dbamv.atendime where cd_atendimento = 654`);
-    console.log(result.rows[0]);
+    // select simples da funão de data e hora atual do banco
+    const result = await connection.execute(`SELECT sysdate FROM dual`);
+    console.log(result.rows);
 
   } catch (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
+    console.error('Erro ao conectar ao Oracle:', err);
   } finally {
     if (connection) {
       try {
@@ -38,4 +35,4 @@ async function run() {
   }
 }
 
-run();
+connectToOracle();
